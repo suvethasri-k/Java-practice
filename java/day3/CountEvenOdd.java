bf774147d1dce34d2606cced70d1f4ab07e023d9
suvethasri <srikamaraj392@gmail.com>
2026-09-25 13:50:55 +0530

    Added CountEvenOdd program

---
+++ java/day3/CountEvenOdd.java
@@ -0,0 +1,21 @@
+class CountEvenOdd {
+    public static void main(String[] args) {
+
+        int arr[] = {10, 15, 20, 25, 30, 35, 40};
+
+        int even = 0;
+        int odd = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+
+            if(arr[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        System.out.println("Even Numbers: " + even);
+        System.out.println("Odd Numbers: " + odd);
+    }
+}
